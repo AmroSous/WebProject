18ac928ded3024a7cb0965796031a7345f4e3127
omarPALE <omar.alratrot@yahoo.com>
2022-12-08 18:08:31 +0200

    prepare to work on workspace

---
+++ jscripts/homepage.js
@@ -0,0 +1,27 @@
+function load(){
+    document.getElementById("img1").style.display="flow";
+}
+function left_s2(){
+    document.getElementById("sec4").style.reduisboreder="5px solid white";
+    document.getElementById("sec4").style.backgroundColor ="white";
+}
+function nleft_s2(){
+    document.getElementById("sec4").style.border="none";
+    document.getElementById("sec4").style.backgroundColor ="rgba(189, 246, 231, 0.2)";
+}
+function left_s3(){
+    document.getElementById("sec5").style.reduisboreder="5px solid white";
+    document.getElementById("sec5").style.backgroundColor ="white";
+}
+function unleft_s3(){
+    document.getElementById("sec5").style.border="none";
+    document.getElementById("sec5").style.backgroundColor ="rgba(189, 246, 231, 0.2)";
+}
+function left_s1(){
+    document.getElementById("sec3").style.reduisboreder="5px solid white";
+    document.getElementById("sec3").style.backgroundColor ="white";
+}
+function nleft_s1(){
+    document.getElementById("sec3").style.border="none";
+    document.getElementById("sec3").style.backgroundColor ="rgba(189, 246, 231, 0.2)";
+}
